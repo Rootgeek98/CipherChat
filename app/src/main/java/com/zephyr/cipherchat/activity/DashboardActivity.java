f53f588a927a7b00e8c14edfa8d2a712fd734777
Bill Glinton <billieglinton@gmail.com>
2021-08-29 18:10:37 +0300

    Fixing Bugs

--- app/src/main/java/com/zephyr/cipherchat/activity/DashboardActivity.java
+++ app/src/main/java/com/zephyr/cipherchat/activity/DashboardActivity.java
@@ -2,15 +2,31 @@ package com.zephyr.cipherchat.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.zephyr.cipherchat.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private Button chatBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        chatBtn = findViewById(R.id.chatBtn);
+
+        chatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, ChatActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
