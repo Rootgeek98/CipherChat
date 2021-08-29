bc6274cd126201f72cb676482e97ab83275234c7
Bill Glinton <billieglinton@gmail.com>
2021-08-29 14:37:40 +0300

    Fixing Bugs and Additional Features

---
+++ app/src/main/java/com/zephyr/cipherchat/app/AppConfig.java
@@ -0,0 +1,19 @@
+package com.zephyr.cipherchat.app;
+
+/**
+ * Contains the URLs in which the app uses to send requests to the server
+ *
+ * @author Bill Glinton
+ * @author Romuald Ashuza
+ * @author Betty Kyalo
+ * @author Kelvin Kimutai
+ * @version 1.0
+ */
+
+public class AppConfig {
+
+    public static final String URL_LOGIN = "http://192.168.0.28/cipher_chat/app/login.php"; // Login URL
+
+    public static final String URL_REGISTER = "http://192.168.0.28/cipher_chat/app/register.php"; // Register URL
+
+}
